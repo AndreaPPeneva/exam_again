7d08fc754860d7eb73829ae29875dd4c26014613
AndreaPeneva <andrea.p.peneva@gmail.com>
2019-03-04 17:12:06 +0100

    exam_again

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function openNav(){
+    document.querySelector(".mobile-nav").style.width="30%";
+}
+  function closeNav(){
+    document.querySelector(".mobile-nav").style.width="0%";
+}
+
+function toggleButton() {
+  document.getElementById("button").className = 'show';
+}
+
+
+
+
+
+
+//function toggleSidebar(){
+//  document.getElementsByClassName("sidebar-contact").classList.toggle('active');
+//}
+
+
+
